38c3603eb31dc3b36d9665a3b1ebb3aacf7577aa
miura-j <miura-j@gnavi.co.jp>
2018-07-17 18:16:29 +0900

    [develop] modelでapi叩くように修正

--- app/controllers/gnavis_controller.rb
+++ app/controllers/gnavis_controller.rb
@@ -1,21 +1,6 @@
+require "gnavis_model.rb"
 class GnavisController < ApplicationController
-    require 'httpclient'
-    require 'yaml'
-    Dotenv.load
     def index
-        client = HTTPClient.new
-        #client.debug_dev = $stderr    # デバッグ情報を標準エラー出力に
-        yaml = YAML.load_file('config/gnavi.yml')
-        url = yaml[Rails.env]['url']
-        id = ''
-        query = {'keyid' => ENV['GNAVI_KEYID'], 'id' => id, 'format' => 'json'}
-        # get
-        res = client.get(url, :query => query, :follow_redirect => true)
-        # post
-        #res = client.post('', :body => query)
-        #puts "code=#{res.code}"    # res.code : Fixnum
-        #puts HTTP::Status.successful?(res.code)
-        #p res.headers
-        render json: res.body
+        render json: GnavisModel.new.get_shopinfo
     end
 end
